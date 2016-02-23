682f6897edb2eef985f041f3c1ad238c8c8c573a
Nick Brooks <nbrooks@thirtyseven.com.au>
2016-02-23 17:43:01 +1030

    Initial Commit

---
+++ IdentityServer/Client.cs
@@ -0,0 +1,38 @@
+﻿using IdentityServer3.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    class ClientConfig
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+                {
+                    new Client
+                    {
+                        Enabled = true,
+                        ClientName = "JS Client",
+                        ClientId = "js",
+                        Flow = Flows.Implicit,
+
+                        RedirectUris = new List<string>
+                        {
+                            "http://localhost:56668/popup.html"
+                        },
+
+                        AllowedCorsOrigins = new List<string>
+                        {
+                            "http://localhost:56668"
+                        },
+
+                        AllowAccessToAllScopes = true
+                    }
+                };
+        }
+    }
+}
---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,35 @@
+﻿using IdentityServer3.Core.Configuration;
+using Owin;
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public object Clients { get; private set; }
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseIdentityServer(new IdentityServerOptions
+            {
+                RequireSsl = false,
+                SiteName = "Embedded IdentityServer",
+                SigningCertificate = LoadCertificate(),
+                EnableWelcomePage = true,
+
+                Factory = new IdentityServerServiceFactory()
+                    .UseInMemoryUsers(Users.Get())
+                    .UseInMemoryClients(ClientConfig.Get())
+                    .UseInMemoryScopes(Scopes.Get())
+            });
+        }
+
+        X509Certificate2 LoadCertificate()
+        {
+            return new X509Certificate2(
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"bin\Certificate\idsrv3test.pfx"), "idsrv3test");
+        }
+    }
+}
